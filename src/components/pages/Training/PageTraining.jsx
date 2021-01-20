4c70fb75e538e526626f5d1973f08f3fb3b505f1
Alexander Cheprasov <a.cheprasov@aic.ru>
2021-01-20 12:16:18 +0700

    refactoring go back in training page

--- src/components/pages/Training/PageTraining.jsx
+++ src/components/pages/Training/PageTraining.jsx
@@ -3,10 +3,20 @@ import css from './PageTraining.module.scss'
 import { Background, ButtonCustom } from '../../common/exportCommon'
 import { PanelHeader } from '@vkontakte/vkui'
 import { useRouter } from '@happysanta/router'
+import { arrTraining } from '../../../redux/constants/otherConstatnts'
 
 export default function PageTraining (props) {
   const router = useRouter()
-  const { title, image, textButton, nextPage } = props
+  const { title, image, textButton, nextPage: page, end } = props
+
+  const nextPage = () => {
+    if (end) {
+      const length = Object.keys(arrTraining).length
+      window.history.go(-length)
+    }
+    else router.pushPage(page)
+  }
+
   return (
     <>
       <PanelHeader
@@ -18,7 +28,7 @@ export default function PageTraining (props) {
         <ButtonCustom
           background={'#14CEDF'}
           text={textButton}
-          onClick={() => router.pushPage(nextPage)}
+          onClick={nextPage}
         />
       </div>
     </>
